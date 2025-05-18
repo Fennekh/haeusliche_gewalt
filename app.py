b3c9e81ddcfb81ed54887ab51d37d8d9e48d2ebc
MRie3000 <maja.riegler@almoo.ch>
2025-05-18 21:01:32 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,32 +1,41 @@
 import dash
 from dash import dcc, html
 import dash_bootstrap_components as dbc
-# Tab-Callback
-
 from dash.dependencies import Input, Output
-
-from layouts import tab_detail_layout, tab_overview_layout  # deine beiden Tabs
+from layouts import tab_zeitliche_layout, tab_geschlecht_layout, tab_trend_layout
 
 # Dash-Instanz
-app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP], suppress_callback_exceptions=True)
+app = dash.Dash(__name__, 
+                external_stylesheets=[dbc.themes.BOOTSTRAP], 
+                suppress_callback_exceptions=True)
 server = app.server
 
 # Layout mit Tabs
 app.layout = html.Div([
+    html.H1("Statistik zu häuslicher Gewalt in der Schweiz",
+            style={'textAlign': 'center', 'color': '#505050', 'marginTop': 20, 'marginBottom': 20}),
+            
     dcc.Tabs(id="tabs", value='tab1', children=[
-        dcc.Tab(label='Übersicht', value='tab1'),
-        dcc.Tab(label='Weitere Analyse', value='tab2'),
+        dcc.Tab(label='Zeitliche Entwicklung', value='tab1'),
+        dcc.Tab(label='Geschlechterverhältnis', value='tab2'),
+        dcc.Tab(label='Trend-Analyse', value='tab3'),
     ]),
     html.Div(id='tabs-content')
 ])
 
-
 @app.callback(Output('tabs-content', 'children'), Input('tabs', 'value'))
 def render_content(tab):
     if tab == 'tab1':
-        return tab_detail_layout.layout
+        return tab_zeitliche_layout.layout
     elif tab == 'tab2':
-        return tab_overview_layout.layout
+        return tab_geschlecht_layout.layout
+    elif tab == 'tab3':
+        return tab_trend_layout.layout
+
+# Registriere die Callbacks aus allen Layout-Modulen
+tab_zeitliche_layout.register_callbacks(app)
+tab_geschlecht_layout.register_callbacks(app)
+tab_trend_layout.register_callbacks(app)
 
 # Starte die App
 if __name__ == '__main__':
